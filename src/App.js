6c2ca7d7a4cb9b4be832d7043c2e0bf5844dde44
Tamara Odom <tamara@Tamaras-MacBook-Pro.local>
2020-08-17 09:51:16 -0400

    pawtastic

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Background from './components/Background';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Background />
+      </div>
+    )
+  }
+
+}
+
+export default App;
---
+++ src/components/Progress.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Progress extends Component {
+    render() {
+        return (
+            <div className="progressArea">
+                <br/> <br/> <br/>
+                <h2>Pawtastic Pet</h2>
+                <br/> <br/> <br/>
+                <p>Human</p>
+                <br/>
+                <p className="current">About pets</p>
+                <br/>
+                <p>Pet details</p>
+                <br/>
+                <p>Confirm</p>
+                <br/> <br/> <br/> <br/>
+                <br/> <br/> <br/> <br/>
+                <a>Exit</a>
+            </div>
+        )
+    }
+}
